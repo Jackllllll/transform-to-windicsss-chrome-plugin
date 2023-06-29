b003596bf36196a639db0722495ad90d1f83b1c5
linzb <lin302010@qq.com>
2023-06-29 17:54:15 +0800

    chore: 增加背景滤镜 backdrop-filter

--- src/toUnocss.ts
+++ src/toUnocss.ts
@@ -7,6 +7,7 @@ import {opacity} from './transform/opacity';
 import {text} from './transform/text';
 import {background} from './transform/background';
 import {border} from './transform/border';
+import {backdrop} from './transform/backdrop';
 
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,7 +20,8 @@ const typeMap: any = {
     opacity,
     text,
     background,
-    border
+    border,
+    backdrop
 };
 // eslint-disable-next-line no-useless-escape
 export const splitReg = /([\w-]+)\s*:\s*([.\w\(\)-\s%+'",#\/!]+)/;
